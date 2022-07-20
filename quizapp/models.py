2e4ec524a5e06e1df0a3816b4a49353dc5c8886d
Ram <kashyap.ram58@gmail.com>
2022-07-20 17:16:47 +0530

    added self to models.py

--- quizapp/models.py
+++ quizapp/models.py
@@ -12,6 +12,8 @@ class Candidate(models.Model):
     state=models.CharField(max_length=22)
     zip=models.CharField(max_length=22)
     date=models.DateField()
+    def __str__(self):
+        return self.email
 
 class Questionmaker(models.Model):
     firstname=models.CharField(max_length=122)
@@ -22,7 +24,10 @@ class Questionmaker(models.Model):
     city=models.CharField(max_length=122)
     state=models.CharField(max_length=22)
     zipc=models.CharField(max_length=22) 
-    date=models.DateField()  
+    date=models.DateField() 
+    def __str__(self):
+        return self.email
+
 
 
 
